87736d7e6157eb88ee67505f1d53193f7438e611
tshirtman <gabriel.pettier@gmail.com>
2011-02-07 13:47:13 +0100

    fixed decorator behaviour (on __name__,  __doc__ and __dict__)

--- pygame_loaders.py
+++ pygame_loaders.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-####################################################################################
+###############################################################################
 # copyright 2008 Gabriel Pettier <gabriel.pettier@gmail.com>
 #
-# This file is part of loaders.py
+# This file is part of pygame_loaders
 # 
-# loaders.py is free software: you can redistribute it and/or modify
+# pygame_loaders is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 # 
-# loaders.py is distributed in the hope that it will be useful,
+# pygame_loaders is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with loaders.py.  If not, see <http://www.gnu.org/licenses/>.
-##################################################################################
+# along with pygame_loaders  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
 
 # standards imports
 import os, sys, copy
@@ -52,6 +52,10 @@ def memoize(function):
             val = function(*args, **kwargs)
             cache[params] = val
             return val
+    decorated_function.__name__ = function.__name__
+    decorated_function.__doc__ = function.__doc__
+    decorated_function.__dict__.update(function.__dict__)
+
     return decorated_function
 
 @memoize #the memoize is critical for performances!
